dbefdd00c0a5af7cd983254608d13d2da55dac80
Warbo360 <129965715+Warbo360@users.noreply.github.com>
2023-08-03 02:29:12 -0500

    Was able to work through the 02 lesson for this assignment
    Only difference was a slight structure difference in how the error mess-
    age is output if the number entered is less than 0, does the same thing
    though. Then I actually had the i = 1 and the conditional i <= num, in
    my for loop but the solution had i = 0, and i < num which results in the
    same thing.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let addedStrings = "";
+    if (num < 0) {
+        addedStrings = "ERROR";
+    }
+    for (i = 0; i < num; i++) {
+        addedStrings += string
+    }
+    return addedStrings
 };
 
 // Do not edit below this line
