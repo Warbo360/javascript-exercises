1d73b9b1d7f0fe8e80164309e51ca5274602e48c
Warbo360 <129965715+Warbo360@users.noreply.github.com>
2023-08-07 04:31:19 -0500

    Was able to complete the 07 assignment with all tests passing.
    Each function utilized one line declaring the variable for the conversion and then the conversion formula and that multiplied by 10 in a Math.round function which itself is then divided by 10 to round to 1 decimal place. Finally the function just returns the value of the variable.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = Math.round(((F-32)*(5/9)) * 10) / 10;
+  return C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = Math.round(((C * (9/5)) + 32) * 10) / 10;
+  return F;
 };
 
 // Do not edit below this line
